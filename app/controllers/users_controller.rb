a27cb955fb016fe478502740c6f504f831b9d142
feruncie <fruncie@chicagobooth.edu>
2024-08-16 01:43:13 +0000

    edited user index and show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < ApplicationController
 
   def index 
-    @users = User.all
+    @the_user = User.all
     render({ :template => "users/index"})
   end 
 
   def show 
-    @users = User.all
-    render({ :template => "users/show"})
+
+    the_id = params.fetch("path_id")
+
+    matching_users = User.where({ :id => the_id })
+
+    @the_user = matching_users.at(0)
+
+    render({ :template => "users/show" })  
   end 
 
 end 
